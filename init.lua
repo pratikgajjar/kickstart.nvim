91a0c8b363b28f42c6af08add0f714770c19db62
Pratik Gajjar <pratik@fampay.in>
2023-01-13 22:29:59 +0530

    Add which key

--- init.lua
+++ init.lua
@@ -42,6 +42,20 @@ require('packer').startup(function(use)
     'nvim-treesitter/nvim-treesitter-textobjects',
     after = 'nvim-treesitter',
   }
+  -- Lua
+  -- I am blind
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 
   -- Git related plugins
   use 'tpope/vim-fugitive'
